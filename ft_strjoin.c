152368d19cb58fb603d88fac08748aaf479c549a
izoukhai <i.zoukhaiev@gmail.com>
2019-04-01 12:41:48 +0200

    push

---
+++ ft_strjoin.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_strjoin.c                                     .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: izoukhai <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/10/06 16:58:29 by izoukhai     #+#   ##    ##    #+#       */
+/*   Updated: 2018/10/06 16:58:30 by izoukhai    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char		*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*res;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	res = !s1 || !s2 ? NULL : ft_strnew(ft_strlen((char*)s1) +
+		ft_strlen((char*)s2));
+	if (!res || !s1 || !s2)
+		return (NULL);
+	while (s1[i])
+		res[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		res[j++] = s2[i++];
+	return (res);
+}
---
+++ ft_strsub.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_strsub.c                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: izoukhai <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/10/06 15:25:47 by izoukhai     #+#   ##    ##    #+#       */
+/*   Updated: 2018/10/06 15:25:50 by izoukhai    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char		*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char			*res;
+	unsigned int	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	res = ft_strnew(len);
+	if (!res)
+		return (NULL);
+	while (s[start + i])
+	{
+		res[i] = s[start + i];
+		i++;
+	}
+	res[len] = '\0';
+	return (res);
+}
